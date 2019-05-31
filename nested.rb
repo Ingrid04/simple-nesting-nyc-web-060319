5e3233c9ddc5ca6b29720aadab671ab335ff25cd
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-31 00:41:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-    programmer_hash[:dennis_ritchie][:languages]
+    puts programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -105,7 +105,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     programmer_hash[:alan_kay][:known_for] = 
+     programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
@@ -124,5 +124,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+ programmer_hash[:dennis_ritchie][:languages] = "1"
 end
